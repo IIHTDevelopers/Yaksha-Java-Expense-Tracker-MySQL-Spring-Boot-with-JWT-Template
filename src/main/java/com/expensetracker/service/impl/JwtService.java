540122a69bcef41e0ca50564392d691d038add34
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-13 19:06:38 +0530

    Update JwtService.java

--- src/main/java/com/expensetracker/service/impl/JwtService.java
+++ src/main/java/com/expensetracker/service/impl/JwtService.java
@@ -13,7 +13,7 @@ import io.jsonwebtoken.Claims;
 @Component
 public class JwtService {
 
-	public static final String SECRET = "";
+	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
 	public String generateToken(String userName) {
 		// write your logic here
